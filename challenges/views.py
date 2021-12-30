45d1ed1e8c7c27a72a29c26fbc50b1aed8761297
Ricardo Pari <rikiricardo16@hotmail.com>
2021-12-30 18:08:41 -0400

    dynamic months

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("this works!")
+def january(request):
+    return HttpResponse("Eat no meat for the entire month!")
 
+
+def february(request):
+    return HttpResponse("Walk for at least 20 minutes every day!")
+
+
+def march(request):
+    return HttpResponse("Learn Django for at least 20 minutes every day!")
+
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = "Eat no meat for the entire month!"
+    elif month == 'february':
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif month == 'march':
+        challenge_text = "Learn Django for at least 20 minutes every day!"
+    else:
+        return HttpResponseNotFound("This month es not supported!")
+    return HttpResponse(challenge_text)
